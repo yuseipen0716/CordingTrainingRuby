7e07918e6ecccf1eb082ec677109e319d9ec54b4
yuseipen0716 <yuseipen1@gmail.com>
2021-10-06 10:34:54 +0900

    Q2-3 done

---
+++ chap02/2-2.rb
@@ -0,0 +1,12 @@
+# 二つの変数xとyの合計と平均を求めて表示するプログラムを作成せよ。なお、xとyには適当な値を代入しておくこと。
+
+def sum_average(x,y)
+    sum = x + y
+    average = (x + y)/2
+    puts "xの値は#{x}です。"
+    puts "xの値は#{y}です。"
+    puts "合計は#{sum}です。"
+    puts "平均は#{average}です。"
+end
+
+sum_average(63,18)
