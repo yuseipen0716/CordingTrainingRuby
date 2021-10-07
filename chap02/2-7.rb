19fe0dafc75f5986c342b95825296f41bec81065
yuseipen0716 <yuseipen1@gmail.com>
2021-10-07 14:12:00 +0900

    add another solution,that I didn't notice.

--- chap02/2-7.rb
+++ chap02/2-7.rb
@@ -13,3 +13,14 @@ split_least_digit = input_number.split(/(.\z)/)
 #splitで配列に格納された値を呼び出し
 puts "最下位桁を除いた値は#{split_least_digit[0]}です。"
 puts "最下位桁は#{split_least_digit[1]}です。"
+
+
+# 解説では乗除演算子を使って求めていた！！
+
+# 乗除演算子を使用して書き直すと ↓ こんな感じだろうか。こちらのほうがスッキリ書けるイメージ
+
+# print '整数値: '
+# input_number = gets.chomp
+
+# puts "最下位桁を除いた値は#{input_number.to_i / 10}です。"
+# puts "最下位桁は#{input_number.to_i % 10}です。"
