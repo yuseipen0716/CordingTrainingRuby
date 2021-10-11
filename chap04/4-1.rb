a1521b171b6ee5a16d3219f9e6391b7c6e7421e3
yuseipen0716 <yuseipen1@gmail.com>
2021-10-12 00:03:39 +0900

    Correct the review points

--- chap04/4-1.rb
+++ chap04/4-1.rb
@@ -8,20 +8,41 @@
 # もう一度？ 1・・・Yes/ 0・・・No : 0
 
 
-print "整数値: "
-number = gets.chomp.to_i
+# print "整数値: "
+# number = gets.chomp.to_i
 
-if number > 0
-    puts 'その値は正です。'
-elsif number == 0
-    puts 'その値は0です。'
-else
-    puts 'その値は負です'
-end
-print "もう一度？ 1・・・Yes/ 0・・・No: "
-answer = gets.chomp.to_i
+# if number > 0
+#     puts 'その値は正です。'
+# elsif number == 0
+#     puts 'その値は0です。'
+# else
+#     puts 'その値は負です'
+# end
+# print "もう一度？ 1・・・Yes/ 0・・・No: "
+# answer = gets.chomp.to_i
 
-until answer == 0
+# until answer == 0
+#     print "整数値: "
+#     number = gets.chomp.to_i
+
+#     if number > 0
+#         puts 'その値は正です。'
+#     elsif number == 0
+#         puts 'その値は0です。'
+#     else
+#         puts 'その値は負です'
+#     end
+#     print "もう一度？ 1・・・Yes/ 0・・・No: "
+#     answer = gets.chomp.to_i
+# end
+
+# untilの中身（25-36)が11-22行目のそのまま写しになっているのをどうにかしたい。11-22行目をsignというメソッドで定義して書いてみようと思ったが、うまくいかなかったので後日違う頭で解き直してみる。
+
+# reviewしていただいた。以下参考にさせていただいたコード
+# isContinuedに初期値を設定
+isContinued = 1
+# isContinued == 1が真となる間はwhile-end間の処理を継続
+while isContinued == 1
     print "整数値: "
     number = gets.chomp.to_i
 
@@ -33,7 +54,12 @@ until answer == 0
         puts 'その値は負です'
     end
     print "もう一度？ 1・・・Yes/ 0・・・No: "
-    answer = gets.chomp.to_i
-end
+    isContinued = gets.chomp.to_i
+    # ここで継続かどうかの判断
 
-# untilの中身（25-36)が11-22行目のそのまま写しになっているのをどうにかしたい。11-22行目をsignというメソッドで定義して書いてみようと思ったが、うまくいかなかったので後日違う頭で解き直してみる。
+    # isContinuedに1,0以外の数値が入れられた場合はメソッドを離脱
+    if isContinued != 1 && isContinued != 0
+        puts '不正な値が入力されましたので処理を終了します。'
+        return
+    end
+end
