3d4c09ba65ff00d66253653d7a4c7b3c373aad46
yuseipen0716 <yuseipen1@gmail.com>
2021-10-13 22:44:02 +0900

    Q4-11 done

---
+++ chap04/4-11.rb
@@ -0,0 +1,30 @@
+# 記号文字を任意の個数だけ出力する問題4-7をfor文で実現せよ
+
+# 実行例
+# 何個*を表示しますか: 12
+# ************
+
+print "何個*を表示しますか: "
+counter = gets.chomp.to_i
+
+if counter > 0
+    i = 1
+    # iがcounterになるまで*を改行なしに出力する繰り返し
+    for i in i..counter
+        i += 1
+        print '*'
+    end
+end
+
+
+# rubyっぽく each文で表現するとこうなるのだろうか。
+# print "何個*を表示しますか: "
+# counter = gets.chomp.to_i
+
+# if counter > 0
+#     i = 1
+#     (i..counter).to_a.each do |i|
+#         i += 1
+#         print '*'
+#     end
+# end
