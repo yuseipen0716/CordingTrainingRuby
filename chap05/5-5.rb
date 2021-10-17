371f8c5c82e988fa02ff833b31bb157d3fe20187
yuseipen0716 <yuseipen1@gmail.com>
2021-10-17 11:08:14 +0900

    add more code

--- chap05/5-5.rb
+++ chap05/5-5.rb
@@ -20,3 +20,22 @@ avg = (num_x + num_y + num_z) / 3
 puts "xとyとzの平均値は#{avg.to_f}です。"
 
 # →この書き方だと、実行例通りに実行すると平均値は8.0 / avgの計算式で / 3.0とすると、平均値はFloatクラスの8.333333333333334となる。
+
+# 実行例
+# 整数値xとyとzの平均値を求めます。
+# xの値: 7
+# yの値: 8
+# zの値: 10
+# xとyとzの平均値は8.333です。
+
+puts '整数値xとyとzの平均値を求めます。'
+print "xの値: "
+num_x =gets.chomp.to_i
+print "yの値: "
+num_y =gets.chomp.to_i
+print "zの値: "
+num_z =gets.chomp.to_i
+
+avg = (num_x + num_y + num_z) / 3.0
+
+puts "xとyとzの平均値は#{avg}です。"
