dfa4b3b6f3551bc8332b404e14461812306972dc
yuseipen0716 <yuseipen1@gmail.com>
2021-10-23 11:35:22 +0900

    Q7-8 done

---
+++ chap07/7-8.rb
@@ -0,0 +1,26 @@
+# a以上b以下の乱数を生成して、その値を返却するメソッドrandomを作成せよ。内部で乱数を生成する標準ライブラリを呼び出すこと。
+# なおbの値がaより小さい場合には、aの値をそのまま返却すること。
+
+# 実行例
+# 乱数を生成します。
+# 下限値 : 10
+# 上限値 : 99
+# 生成した乱数は55です。
+
+def random(a, b)
+    # 文字などが入力された場合にはreturn
+    return unless a.match(/\d/) && b.match(/\d/)
+    a = a.to_i
+    b = b.to_i
+    num = rand(a..b)
+    num = "#{a}(入力された下限値が上限値を超えていたため、下限値を出力しています。)" if a > b
+    puts "生成した乱数は#{num}です。"
+end
+
+puts '乱数を生成します。'
+print "下限値 : "
+min = gets.chomp
+print "上限値 : "
+max = gets.chomp
+
+random(min, max)
