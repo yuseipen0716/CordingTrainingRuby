d7234428b94de60b76bce944a66d0473972ac638
yuseipen0716 <yuseipen1@gmail.com>
2021-10-23 10:09:36 +0900

    Q7-3 done

---
+++ chap07/7-3.rb
@@ -0,0 +1,16 @@
+# 三つのint型引数a, b, cの中央値を求めるメソッドmedを作成せよ
+
+# 入力される数値は整数値という前提で実装
+def med(a, b, c)
+    # 引数は三つの整数値であるため、配列をsortして真ん中の要素を取り出す。
+    puts "中央値は#{[a, b, c].sort[1]}です。"
+end
+
+print "整数a : "
+a = gets.chomp.to_i
+print "整数b : "
+b = gets.chomp.to_i
+print "整数c : "
+c = gets.chomp.to_i
+
+med(a, b, c)
