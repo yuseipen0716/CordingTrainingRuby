ced7a803d00fcfe7f29972713e3518598c5160e7
yuseipen0716 <yuseipen1@gmail.com>
2021-10-29 00:24:18 +0900

    correct for review points

--- chap06/6-12.rb
+++ chap06/6-12.rb
@@ -13,21 +13,39 @@
 print "要素数 : "
 element = gets.chomp.to_i
 
+# if element <= 10
+#     # 乱数を配列aに格納（まず、配列の最初の要素だけ設定）
+#     a = []
+#     a << rand(1..10)
+#     puts "a[#{0}] = #{a[0]}"
+#     # 配列の2つ目の要素以降はここで繰り返し処理
+#     (1..element-1).each do |i|
+#         random = rand(1..10)
+#         # 22行目でrandomという変数を1〜10の乱数で初期化。selectメソッドを利用して配列の中に同じ要素が入っていないかを評価して、同じ要素が入っている場合はuntil以降の繰り返しへ。
+#         # 配列に同じ数値が入っていなければ(selectメソッドの戻り値が空の配列[]となる場合）、if文は抜けて a << randomへ
+#         if a.select { |num| num == random } != []
+#             until a.select { |num| num == random } == []
+#                 random = rand(1..10)
+#             end
+#         end
+#         a << random
+#         puts "a[#{i}] = #{a[i]}"
+#     end
+# else
+#     puts '要素数は10以下の正の整数で入力してください。'
+# end
+
+# レビューしていただきました。
 if element <= 10
-    # 乱数を配列aに格納（まず、配列の最初の要素だけ設定）
     a = []
     a << rand(1..10)
-    puts "a[#{0}] = #{a[0]}"
-    # 配列の2つ目の要素以降はここで繰り返し処理
-    (1..element-1).each do |i|
+    (0..element-1).each do |i|
         random = rand(1..10)
-        # 22行目でrandomという変数を1〜10の乱数で初期化。selectメソッドを利用して配列の中に同じ要素が入っていないかを評価して、同じ要素が入っている場合はuntil以降の繰り返しへ。
-        # 配列に同じ数値が入っていなければ(selectメソッドの戻り値が空の配列[]となる場合）、if文は抜けて a << randomへ
-        if a.select { |num| num == random } != []
-            until a.select { |num| num == random } == []
-                random = rand(1..10)
-            end
+        # 配列aに↑で定義したrandomが含まれている間はrandomを初期化し続ける
+        while a.include?(random)
+            random = rand(1..10)
         end
+        # whileを抜けられたらrandomをaに格納。
         a << random
         puts "a[#{i}] = #{a[i]}"
     end
